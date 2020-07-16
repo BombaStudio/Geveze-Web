68fb10a0fda18e79a61876d18a8bdcad5241bcda
GiantProgrammer <37694356+BombaStudio@users.noreply.github.com>
2020-07-17 00:06:46 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function build(ty, id){
+    
+    if (ty == "circle"){
+        playground.innerHTML += "<div class='circle' id='"+id+"'></div>";
+    }
+    if (ty == "rect"){
+        playground.innerHTML += "<div class='rect' id='"+id+"'></div>";
+    }
+    if (ty == "axis2"){
+        playground.innerHTML += "<div class='ax'></div>";
+        playground.innerHTML += "<div class='ay'></div>";
+    }
+    eval("var "+id+" = document.getElementById("+id+")");
+}
+function del(obj){
+    eval(obj+".remove()");
+}
+function design(obj, sty){
+    eval(obj+".style."+sty);
+}
+function run(){
+    var playground = document.getElementById("playground");
+    var code = document.getElementById("code").value;
+    eval(code);
+}
